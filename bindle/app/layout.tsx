db6034c0f0c72b701c51b70dbc950ab33fa75614
Patrick <pjr99@cornell.edu>
2025-02-27 12:30:19 -0500

    Fixed UI

--- bindle/app/layout.tsx
+++ bindle/app/layout.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css';
+import '@/app/styles/globals.css';
 import { Inter } from 'next/font/google';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
